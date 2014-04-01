0e01110d334c57f691aaeaa40dafb56245457df8
J. Andrew Brassington <jabbrass@zoho.com>
2014-04-01 11:59:46 -0500

    Removed LetterChanges Function

--- HigherOrderProject/library.js
+++ HigherOrderProject/library.js
@@ -7,27 +7,6 @@ function saveName(firstnameFunc, lastname, middlename) {
     document.getElementById("output").innerHTML = combinedName;
 }
 
-
-function LetterChanges(str) {
-
-    var result = "";
-    for (var i = 0; i < str.length; i++) {
-        var current = str[i];
-        if (str[i] == 'z') {
-            current = 'a';
-        } else if (str.charCodeAt(i) >= 'a'.charCodeAt(0) && str.charCodeAt(i) <= 'z'.charCodeAt(0)) {
-            current = String.fromCharCode(str.charCodeAt(i) + 1);
-            //cur = 'X';
-        }
-
-        if (current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u') {
-            current = current.toUpperCase();
-        }
-        result += current;
-    }
-    return result;
-}
-
 function SimpleAdding(num) {
     return num * (num + 1) / 2;
 }
